89ad0056423c9ec93afb8ba04a77e458ceebed38
Reshetov Denis <reshetovdenis@gmail.com>
2023-08-11 10:45:42 +0200

    Providing data to storage

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -3,7 +3,7 @@ use super::*;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
-pub const LAW_PRICE: u64 = 10;
+pub const LAW_PRICE: u32 = 10;
 pub const INITIAL_LAW_ID: [u8; 32] = [
     212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133,
     76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125,
@@ -17,22 +17,26 @@ pub const EDITED_LAW_TEXT: [u8; 32] = [
 benchmarks! {
 	create {
 		let caller = whitelisted_caller();
-		let law_price: BalanceOf<T> = 0u32.into();
+		let law_price: BalanceOf<T> = LAW_PRICE.into();
 	}: _(RawOrigin::Signed(caller), INITIAL_LAW_ID, law_price)
 	edit {
 		let caller = whitelisted_caller();
-		let law_price: BalanceOf<T> = 0u32.into();
+		let law_price: BalanceOf<T> = LAW_PRICE.into();
+		Pallet::<T>::create(RawOrigin::Signed(caller.clone()).into(), INITIAL_LAW_ID, law_price)?;
 	}: _(RawOrigin::Signed(caller), INITIAL_LAW_ID, EDITED_LAW_TEXT, law_price)
 	upvote {
 		let caller = whitelisted_caller();
-		let law_price: BalanceOf<T> = 0u32.into();
+		let law_price: BalanceOf<T> = LAW_PRICE.into();
+		Pallet::<T>::create(RawOrigin::Signed(caller.clone()).into(), INITIAL_LAW_ID, law_price)?;
 	}: _(RawOrigin::Signed(caller), INITIAL_LAW_ID, law_price)
 	downvote {
 		let caller = whitelisted_caller();
-		let law_price: BalanceOf<T> = 0u32.into();
+		let law_price: BalanceOf<T> = LAW_PRICE.into();
+		Pallet::<T>::create(RawOrigin::Signed(caller.clone()).into(), INITIAL_LAW_ID, law_price)?;
 	}: _(RawOrigin::Signed(caller), INITIAL_LAW_ID, law_price)
 	remove {
 		let caller = whitelisted_caller();
+		Pallet::<T>::create(RawOrigin::Signed(caller.clone()).into(), INITIAL_LAW_ID, law_price)?;
 	}: _(RawOrigin::Signed(caller), INITIAL_LAW_ID)
 }
 
